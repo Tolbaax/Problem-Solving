d9ea5acd292722be0e23d619f47d27e78980ca87
Mohamed Tolba <Tolbaax@gmail.com>
2022-11-28 23:29:25 +0200

    P2

--- lib/easy/problem_2.dart
+++ lib/easy/problem_2.dart
@@ -10,7 +10,7 @@ class Solution2 {
 }
 
 /*
- ``Hash Table``
+ ``Hash Table`` => not used
  This idea uses hash table to record the times of appearances of each letter in th two
  Strings 's' and 't'. For each letter in 's' it increase the counter by 1 while for each
  letter in 't', it decreases the counter by 1. Finally, all the counters will be 0 if
