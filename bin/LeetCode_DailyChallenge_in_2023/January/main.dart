693b3c248452ed51e1bb722a77f44b45c75608c6
Mohamed Tolba <Tolbaax@gmail.com>
2023-01-05 23:13:12 +0200

    Updating..

--- bin/LeetCode_DailyChallenge_in_2023/January/main.dart
+++ bin/LeetCode_DailyChallenge_in_2023/January/main.dart
@@ -1,3 +1,5 @@
+import 'package:problem_solving/LeetCode_DailyChallenge_in_2023/January/problems/3-Delete_Columns_to_Make_Sorted.dart';
+
 import 'app.dart';
 
 void main() {
@@ -6,4 +8,7 @@ void main() {
 
   // Problem 2
   print('S2 : ${Solution2().detectCapitalUse('WoRd')}');
+
+  // Problem 3
+  print('S3 : ${Solution3().minDeletionSize(["ass", "frs", "qws"])}');
 }
