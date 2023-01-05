e1f3d9a2fb1263348e376b0c6010642840270b66
Mohamed Tolba <Tolbaax@gmail.com>
2023-01-05 21:50:45 +0200

    Updating..

--- bin/LeetCode_DailyChallenge_in_2023/January/app.dart
+++ bin/LeetCode_DailyChallenge_in_2023/January/app.dart
@@ -1,2 +1,2 @@
-export 'package:problem_solving/LeetCode_DailyChallenge_in_2023/January/1-word_pattern.dart';
-export 'package:problem_solving/LeetCode_DailyChallenge_in_2023/January/2-detect_capital.dart';
+export 'package:problem_solving/LeetCode_DailyChallenge_in_2023/January/problems/1-word_pattern.dart';
+export 'package:problem_solving/LeetCode_DailyChallenge_in_2023/January/problems/2-detect_capital.dart';
