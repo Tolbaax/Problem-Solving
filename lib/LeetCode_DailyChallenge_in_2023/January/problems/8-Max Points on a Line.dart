cd6f65c99ed939969db780c97520d1a197e5a2a4
Mohamed Tolba <Tolbaax@gmail.com>
2023-01-08 22:09:42 +0200

    Updating..

--- lib/LeetCode_DailyChallenge_in_2023/January/problems/8-Max Points on a Line.dart
+++ lib/LeetCode_DailyChallenge_in_2023/January/problems/8-Max Points on a Line.dart
@@ -3,19 +3,25 @@ import 'dart:math';
 class Solution8 {
   int maxPoints(List<List<int>> points) {
     int n = points.length, ans = 1;
-    if (n < 2) return n;
     for (int i = 0; i < n - 1; i++) {
-      // to store our slope value
+      // store <slope,that selected point>
       Map<double, int> mp = {};
       for (int j = i + 1; j < n; j++) {
-        if (i == j) continue;
-        var dy = points[j][1] - points[i][1]; //y2 - y1
-        var dx = points[j][0] - points[i][0]; //y2 - y1
-        // convert both arguments to doubles.
-        double x = atan2(dy, dx);
-        mp[x] = (mp[x] ?? 0) + 1;
-        ans = max(ans, mp[x]! + 1);
+        if (points[j][1] == points[i][1]) {
+          mp[-90] = (mp[-90] ?? 0) + 1;
+        } else {
+          var dy = (points[j][0] - points[i][0]);
+          var dx = (points[j][1] - points[i][1]);
+          double slope = dy / dx;
+          // count the number of points that have the same slope
+          mp[slope] = (mp[slope] ?? 0) + 1;
+        }
       }
+      int temp = 0;
+      for (MapEntry<double, int> it in mp.entries) {
+        temp = max(temp, it.value + 1);
+      }
+      ans = max(temp, ans);
     }
     return ans;
   }
